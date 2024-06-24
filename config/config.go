e56f92e6874c0bb7dedd514294142f2268564004
Matias Bontempo <matias.bontempo@gmail.com>
2024-06-23 22:28:20 -0300

    feat: Added default values for env variables

--- config/config.go
+++ config/config.go
@@ -9,13 +9,13 @@ import (
 )
 
 var (
-	PORT                      string
-	ENV                       string
-	WEB_URL                   string
-	REDIS_URL                 string
-	REDIS_CACHE_EXPIRATION    string
-	RESPONSE_CACHE_EXPIRATION string
-	CORE_CACHE_EXPIRATION     int
+	PORT                      = "3000"
+	ENV                       = "development"
+	WEB_URL                   = "http://localhost:5173"
+	REDIS_URL                 = "redis://localhost:6379"
+	REDIS_CACHE_EXPIRATION    = "28800"
+	RESPONSE_CACHE_EXPIRATION = "3600"
+	CORE_CACHE_EXPIRATION     = 0
 )
 
 func getEnv(key, defaultValue string) string {
@@ -29,7 +29,7 @@ func getEnv(key, defaultValue string) string {
 func Init() {
 	err := godotenv.Load()
 	if err != nil {
-		logger.LogFatal("Error loading .env file")
+		logger.LogWarn("Error loading .env file")
 	}
 
 	PORT = getEnv("PORT", "3000")
