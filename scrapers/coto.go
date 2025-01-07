9f58366dfb1a8b4ef572ed20f1a69e9815eebcf1
Matias Bontempo <matias.bontempo@gmail.com>
2025-01-06 22:56:12 -0300

    feat: updated coto scrapper

--- scrapers/coto.go
+++ scrapers/coto.go
@@ -1,62 +1,104 @@
 package scrapers
 
 import (
+	"encoding/json"
+	"fmt"
+	"strconv"
 	"strings"
 
-	"ratoneando/cores/html"
+	"ratoneando/cores/api"
 	"ratoneando/products"
-	"ratoneando/utils/numbers"
+	"ratoneando/utils/logger"
 )
 
-func coalesce(strs ...string) string {
-	for _, str := range strs {
-		if str != "" {
-			return str
-		}
-	}
-	return ""
+type CotoResponseProduct struct {
+	DetailsAction struct {
+		RecordState string `json:"recordState"`
+	} `json:"detailsAction"`
+	Attributes struct {
+		ProductDisplayName []string `json:"product.displayName"`
+		ProductRepositoryId []string `json:"product.repositoryId"`
+	} `json:"attributes"`
+	Records []struct {
+		Attributes struct {
+			SkuReferencePrice []string `json:"sku.referencePrice"`
+			SkuActivePrice []string `json:"sku.activePrice"`
+			ProductContent []string `json:"product.CONTENIDO"`
+			ProductMediumImageUrl []string `json:"product.mediumImage.url"`
+			SkuQuantity []string `json:"sku.quantity"`
+			ProductDiscounts []string `json:"product.dtoDescuentos"`
+		} `json:"attributes"`
+	} `json:"records"`
 }
 
-func extractor(element *html.ElementWrapper, doc *html.DocumentWrapper) products.ExtendedSchema {
-	id, _ := element.Attr("id")
-	id = strings.Replace(id, "li_prod", "", -1)
+type CotoProductDiscounts []struct {
+	precioDescuento string
+}
 
-	name := element.Find("div.descrip_full").Text()
+type CotoRawProduct struct {
+	CotoResponseProduct
+	CotoProductDiscounts
+}
 
-	// rawPrice := coalesce(element.Find(".price_discount").Text(), element.Find(".info_discount span.atg_store_newPrice").Text())
-	rawPrice := coalesce(
-		strings.ReplaceAll(element.Find(".price_discount_gde").Text(), ".", ","),
-		element.Find("span.atg_store_newPrice").First().Text(),
-	)
-
-	price, _ := numbers.ParseMoney(rawPrice)
-
-	link, _ := element.Find("a").Attr("href")
-	link = "https://www.cotodigital3.com.ar" + link
-
-	image, _ := element.Find("span.atg_store_productImage > img").Attr("src")
-	unavailable := element.Find("div.product_not_available").Length() > 0
-
-	return products.ExtendedSchema{
-		ID:          id,
-		Name:        name,
-		Link:        link,
-		Image:       image,
-		Price:       price,
-		Unavailable: unavailable,
-	}
+type CotoResponseStructure struct {
+	Contents []struct {
+		Main []struct {
+			Contents []struct {
+				Records []CotoResponseProduct `json:"records"`
+			} `json:"contents"`
+		} `json:"Main"`
+	} `json:"contents"`
 }
 
 func Coto(query string) ([]products.Schema, error) {
-	return html.Core(html.CoreProps{
-		Query:   query,
-		BaseUrl: "https://www.cotodigital3.com.ar",
-		Source:  "coto",
-		SearchPattern: func(q string) string {
-			return "/sitios/cdigi/browse?Ntt=" + q + "&_DARGS=%2Fsitios%2Fcartridges%2FSearchBox%2FSearchBox.jsp"
+	return api.Core(api.CoreProps[CotoResponseStructure, CotoRawProduct]{
+		Query:         query,
+		BaseUrl:       "https://www.cotodigital.com.ar",
+		SearchPattern: func(q string) string { return "/sitios/cdigi/categoria?Ntt=" + q + "&format=json" },
+		Source:        "coto",
+		Normalizer: func(response CotoResponseStructure) []CotoRawProduct {
+			var normalizedProducts []CotoRawProduct
+
+			for _, rawProduct := range response.Contents[0].Main[1].Contents[0].Records {
+        var productData CotoProductDiscounts
+
+				if len(rawProduct.Records[0].Attributes.ProductDiscounts) == 0 {
+					continue
+				}
+
+				err := json.Unmarshal([]byte(rawProduct.Records[0].Attributes.ProductDiscounts[0]), &productData)
+
+				if err != nil {
+					logger.LogWarn(fmt.Sprintf("Error unmarshalling product data: %s", err))
+				}
+
+				normalizedProducts = append(normalizedProducts, CotoRawProduct{
+					CotoResponseProduct: rawProduct,
+					CotoProductDiscounts: productData,
+				})
+			}
+
+			return normalizedProducts
 		},
-		ContainerSelector: "ul#products",
-		ProductSelector:   "li[id^='li_prod']",
-		Extractor:         extractor,
+		Extractor: func(rawProduct CotoRawProduct) products.ExtendedSchema {
+      listPrice, _ := strconv.ParseFloat(rawProduct.Records[0].Attributes.SkuActivePrice[0], 64)
+      var price float64 = listPrice
+      if len(rawProduct.CotoProductDiscounts) > 0 {
+          price, _ = strconv.ParseFloat(rawProduct.CotoProductDiscounts[0].precioDescuento, 64)
+      }
+
+      fmt.Println(price)
+      fmt.Println(listPrice)
+			return products.ExtendedSchema{
+				ID:          rawProduct.CotoResponseProduct.Attributes.ProductRepositoryId[0],
+				Source:      "coto",
+				Name:        rawProduct.CotoResponseProduct.Attributes.ProductDisplayName[0],
+				Link:        strings.Replace(rawProduct.DetailsAction.RecordState, "?format=json", "", -1),
+				Image:       rawProduct.Records[0].Attributes.ProductMediumImageUrl[0],
+				Unavailable: rawProduct.Records[0].Attributes.SkuQuantity[0] == "0",
+				Price:       price,
+				ListPrice:   listPrice,
+			}
+		},
 	})
 }
